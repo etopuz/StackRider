5f00da4b851fe619d3f7a78dc3b70e2c35ed1253
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-01 01:08:59 +0300

    add: collectibles (gem, ball)

---
+++ Assets/Scripts/Collectible/Ball.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour, ICollectible
+{
+    private BoxCollider _collider;
+    private bool _isStacked;
+    private void Start()
+    {
+        _collider = GetComponent<BoxCollider>();
+    }
+    
+
+    public void OnCollect()
+    {
+        StackManager.Instance.Pickup(transform);
+        _collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Collectible/Gem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SimpleCollectibleScript : MonoBehaviour, ICollectible
+{
+
+	[Header("Rotation")]
+	[SerializeField] private bool rotate; // do you want it to rotate?
+	[SerializeField] private float rotationSpeed;
+	
+
+	void Update () {
+
+		if (rotate)
+			transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+
+	}
+
+	public void OnCollect()
+	{
+		AudioManager.Instance.PlayCollectAudio(transform.position);
+		Destroy(gameObject);
+	}
+}
