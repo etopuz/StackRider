1135b767a6700f5c04b0cc45e51ddeed9dc9c204
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-01 20:16:55 +0300

    add: staticobstacle class

---
+++ Assets/Scripts/Obstacles/StaticObstacle.cs
@@ -0,0 +1,13 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace StackRider.Obstacles
+{
+    public class StaticObstacle : IObstacle
+    {
+        public void DropBall()
+        {
+            
+        }
+    }
+}
