71f64a70dc6757da49ac7bfc97c1c64160e7c33a
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-01 20:16:43 +0300

    add: iobstacle interface

---
+++ Assets/Scripts/Interfaces/IObstacle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace StackRider
+{
+    public interface IObstacle
+    {
+        public void DropBall();
+    }
+}
+
