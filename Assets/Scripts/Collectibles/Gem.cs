e0a0be0282aa9db18fe087c25d661ad124fd7fdd
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-05 02:30:48 +0300

    update: now collected gems are shown on UI

--- Assets/Scripts/Collectibles/Gem.cs
+++ Assets/Scripts/Collectibles/Gem.cs
@@ -1,24 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace StackRider.Collectibles
 {
     public class Gem : MonoBehaviour, ICollectible
     {
 
-        [Header("Rotation")] [SerializeField] private bool rotate; // do you want it to rotate?
+        [Header("Rotation")] 
+        [SerializeField] private bool rotate; // do you want it to rotate?
         [SerializeField] private float rotationSpeed;
 
+        private GameManager _gameManager;
 
-        void Update()
+        private void Start()
         {
+            _gameManager = GameManager.Instance;
+        }
 
+        private void Update()
+        {
             if (rotate)
+            {
                 transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
-
+            }
+                
         }
 
         public void OnCollect()
         {
+            _gameManager.AddGem();
             //AudioManager.Instance.PlayCollectAudio(transform.position);
             Destroy(gameObject);
         }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,20 @@
+using UnityEngine;
+
 namespace StackRider
 {
     public class GameManager : Singleton<GameManager>
     {
         public GameState state = GameState.Playing;
+
+        [SerializeField] private GameStatsUI gameStatsUI;
+        private int _numberOfGemsCollected = 0;
+
+        public void AddGem()
+        {
+            _numberOfGemsCollected++;
+            gameStatsUI.SetGemText(_numberOfGemsCollected);
+        }
+        
     }
 
     public enum GameState
