cf17dff8a36ebe242d9b1d10c0dbe2468254f140
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-06 02:54:54 +0300

    update: now balls are dropped when win game

--- Assets/Scripts/Managers/StackManager.cs
+++ Assets/Scripts/Managers/StackManager.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Buffers;
+using System.Collections;
 using System.Collections.Generic;
 using StackRider.Collectibles;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -9,6 +12,8 @@ namespace StackRider
 {
     public class StackManager : Singleton<StackManager>
     {
+        public int NumberOfBalls => _stack.Count;
+        
         [Header("Player Models")]
         [SerializeField] private Transform mainBall;
         [SerializeField] private Transform character;
@@ -18,26 +23,35 @@ namespace StackRider
         [SerializeField] private Transform freeBallsContainer;
 
         [Header("Stack")] 
+        [SerializeField] private float ballDroppingTimeWhenSuccess;
         [SerializeField] private float ballRotationSpeed; 
         public int maxStackAmount = 20;
-
+        
+        private float passedTimeInBetweenBallDrops;
         private float _distanceBetweenBalls;
         private List<Transform> _stack = new List<Transform>();
-        public int NumberOfBalls => _stack.Count;
 
         private GameManager _gameManager;
+        private LevelManager _levelManager;
         private void Start()
         {
             _gameManager = GameManager.Instance;
+            _levelManager = LevelManager.Instance;
             _distanceBetweenBalls = mainBall.localScale.y;
             _stack.Add(mainBall);
         }
 
         private void Update()
         {
-            if (_gameManager.state == GameState.Playing)
+            switch (_gameManager.state)
             {
-                RotateBallsOnStack();
+                case GameState.Playing:
+                    RotateBallsOnStack();
+                    break;
+                
+                case GameState.Success when NumberOfBalls > 0:
+                    DropBallsWhenSuccess();
+                    break;
             }
         }
 
@@ -64,7 +78,7 @@ namespace StackRider
             int removeIndex = _stack.Count - 1;
             if (numberOfDroppedBalls > removeIndex)
             {
-                _gameManager.Fail();
+                StartCoroutine(_levelManager.RestartLevelAfterWait());
             }
 
             else
@@ -89,6 +103,26 @@ namespace StackRider
                 rotateDirection *= -1;
             }
         }
+
+        public void DropBallsWhenSuccess()
+        {
+            passedTimeInBetweenBallDrops += Time.deltaTime;
+            
+            if (passedTimeInBetweenBallDrops >= ballDroppingTimeWhenSuccess)
+            {
+                passedTimeInBetweenBallDrops = 0f;
+                _stack[^1].gameObject.SetActive(false);
+                _stack.RemoveAt(NumberOfBalls - 1);
+                ShiftStack();
+            }
+
+            if (NumberOfBalls == 0)
+            {
+                Transform player = _gameManager.playerObj.transform;
+                player.localPosition = new Vector3(player.localPosition.x, 0f, player.localPosition.z);
+            }
+        }
+        
     }
 }
 
