77815d375d3fe410649d5b25e079297e0577827d
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-06 01:04:27 +0300

    add: tagLayerData

---
+++ Assets/Scripts/Utils/TagLayerData.cs
@@ -0,0 +1,12 @@
+namespace StackRider
+{
+    public static class TagLayerData
+    {
+        // STATS
+        public const string LEVEL = "Level";
+        public const string GEM = "Gem";
+        
+    }
+}
+
+
