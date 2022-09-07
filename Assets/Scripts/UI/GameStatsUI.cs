f2bedcf41960ac1b3fca95bbcd924e6ac7ac7bf0
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-09-08 02:21:46 +0300

    use: tmp extension to optimize code

--- Assets/Scripts/UI/GameStatsUI.cs
+++ Assets/Scripts/UI/GameStatsUI.cs
@@ -4,31 +4,27 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class GameStatsUI : MonoBehaviour
-{
-    [SerializeField] private TextMeshProUGUI gemCountText;
-    [SerializeField] private TextMeshProUGUI levelCountText;
-
-    private GameManager _gameManager;
-    private LevelManager _levelManager;
-    
-    
-    
-    private void Awake()
+namespace StackRider.UI{
+    public class GameStatsUI : MonoBehaviour
     {
-        _gameManager = GameManager.Instance;
-        _levelManager = LevelManager.Instance;
-    }
+        [SerializeField] private TextMeshProUGUI gemCountText;
+        [SerializeField] private TextMeshProUGUI levelCountText;
 
-    private void Update()
-    {
-        SetText(gemCountText,(_gameManager.numberOfGemsCollected + _levelManager.collectedGems).ToString());
-        SetText(levelCountText, TagLayerData.LEVEL + " " + (_levelManager.levelIndex).ToString());
-    }
-    private void SetText(TextMeshProUGUI tmp, string message)
-    {
-        tmp.text = message;
-    }
+        private GameManager _gameManager;
+        private LevelManager _levelManager;
 
+        private void Awake()
+        {
+            _gameManager = GameManager.Instance;
+            _levelManager = LevelManager.Instance;
+        }
 
+        private void Update()
+        {
+            levelCountText.SetText(_levelManager.levelIndex, "LEVEL ", null);
+            gemCountText.SetText(_gameManager.numberOfGemsCollected + _levelManager.collectedGems);
+        }
+        
+    }
 }
+
